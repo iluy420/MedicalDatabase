5cead72d7e4acf6b079baff5aeae36398e7ff746
iluy420 <iluy.marckov@mai.ru>
2021-07-18 19:12:38 +0300

    v0.1

---
+++ MedicalData/MedicalData/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace MedicalData
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<DateMeds> DateMed { get; set; }
+        public ApplicationContext() : base("DefaultConnection") { }
+    }
+}
---
+++ MedicalData/MedicalData/DateMeds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalData
+{
+    class DateMeds
+    {
+        private int PatientNumber { get; set; }
+        private string Surname, Name, Patronymic;
+
+        public DateMeds() { }
+        public DateMeds(string Surname, string Name, string Patronymic) {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Patronymic = Patronymic;
+        }
+    }
+}
